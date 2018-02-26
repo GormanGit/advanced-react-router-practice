ea801102aca64d107cd445b128543268dd921857
MrUser <user1@localhost.localdomain>
2018-02-26 08:04:15 -0600

    some work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Charts from "./components/Charts";
+import Tables from "./componentsTables";
+import Settings from "./components/Settings";
+import Wall from "./components/Wall";
+import Marquee from "./components/Marquee";
+import Profiles from "./components/Profiles";
+import Profile from "./components/Profile";
+import Dashboard from "./components/Dashboard";
+
+
+
+
+
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div> 
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -12,9 +28,20 @@ function App() {
         </nav>
         <div style={{backgroundColor: "white"}}>
           {/* PUT YOUR ROUTES HERE */}
+          <Switch>
+            <Route path="/charts" components={Charts} />
+            <Route path="/tables" components={Tables} />
+            <Route path="/settings" components={Settings} />
+            <Route path="/wall" components={Wall} />
+            <Route path="/profiles" components={Profiles} />
+            <Route path="/marquee/:text" components={Marquee} />
+            <Route path="/profile/:id" components={Profile} />
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </div>
     </div>
+    </BrowserRouter>
 
   );
 }
